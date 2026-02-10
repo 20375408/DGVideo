b9421fb713ddfdca292c581037f2a4c0cabfa8bd
20375408 <20375408@qq.com>
2026-02-10 10:09:39 +0800

    Update placeholder text in SearchBox component

--- components/search/SearchBox.tsx
+++ components/search/SearchBox.tsx
@@ -13,7 +13,7 @@ interface SearchBoxProps {
     placeholder?: string;
 }
 
-export function SearchBox({ onSearch, onClear, initialQuery = '', placeholder = '内容独家定制qq20375408，本搜索框可搜索电影、电视剧、综艺...' }: SearchBoxProps) {
+export function SearchBox({ onSearch, onClear, initialQuery = '', placeholder = '搜索⤵︎定制qq20375408，本框可搜索电影、电视剧、综艺...' }: SearchBoxProps) {
     const [query, setQuery] = useState(initialQuery);
     const inputRef = useRef<HTMLInputElement>(null);
 
